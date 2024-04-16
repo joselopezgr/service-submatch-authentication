209f3b6caa9868435ba838e9d3998c1720cb7578
Jose Lopez <jose.guerrero@tes.com>
2024-04-16 15:18:03 +0100

    secretKeyVar

--- security/src/main/java/com/jlg/submatch/service/user/service/JwtService.java
+++ security/src/main/java/com/jlg/submatch/service/user/service/JwtService.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,8 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String secretKey = "D6FDE26645E8ADB167A8CA33B5358";
+    @Value("${jwt.secret.key")
+    private static String secretKey;
 
     public String extractUsername(String jwtToken) {
         return extractClaim(jwtToken, Claims::getSubject);
